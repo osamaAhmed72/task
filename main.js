ff3a9e6420b3b29620bfb41ebc320d94eca0c913
osamaAhmed72 <osamawebdeveloper7@gmail.com>
2025-05-24 23:08:21 +0300

    edit

---
+++ main.js
@@ -0,0 +1,11 @@
+let sidenav = document.getElementById('sidenavShow');
+
+function showSideNav(){
+
+
+    if (sidenav.style.display === 'none') {
+        sidenav.style.display = 'block';
+    } else {
+        sidenav.style.display = 'none';
+    }
+}
